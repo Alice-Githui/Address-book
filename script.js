8d0687777f6fa1fff9cb5273fd75b2ad251454c4
Alice-Githui <gnyokabi.alice@gmail.com>
2021-03-02 23:13:22 +0300

    Used the example on the LMS -canvas - to check on my forms later

--- script.js
+++ script.js
@@ -1,11 +1,49 @@
-//using Concat as a constructor to create a 'roadmap' that will be used to create new entries in the address book
+//using Contact as a constructor to create a 'roadmap' that will be used to create new entries in the address book
 
-function Concat (firstName, lastName){
+function Contact (firstName, lastName){
     this.firstName = firstName;
     this.lastName = lastName;
 }
 
-let Anna = new Concat ("Anna", "McHughes")
-
+let Anna = new Contact ("Anna", "McHughes")
 console.log(Anna.firstName)
-console.log(Anna.lastName);
+/*
+console.log(Anna.firstName)
+console.log(Anna.lastName); */
+
+//user-interface logic 
+
+/*$("#contactform").submit(function(event){
+    event.preventDefault();
+    
+    let inputtedFirstName = $("input#First").val();
+    let inputtedLastName = $("input#Last").val();
+
+   // console.log(Anna.firstName)
+
+    let newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $("ul#contacts").append("<li><span class ='contact'>" + newContact.firstName + "</span></li>");
+
+    $("input#First").val(" ");
+    $("input#Last").val(" ")
+}); */
+
+
+
+
+$(document).ready(function() {
+    $("form#new-contact").submit(function(event) {
+      event.preventDefault();
+  
+      var inputtedFirstName = $("input#new-first-name").val();
+      var inputtedLastName = $("input#new-last-name").val();
+  
+      var newContact = new Contact(inputtedFirstName, inputtedLastName);
+  
+      $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+  
+      $("input#new-first-name").val("");
+      $("input#new-last-name").val("");
+    });
+  });  
